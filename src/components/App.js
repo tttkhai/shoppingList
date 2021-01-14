03e0e425a76e1542901f24eb3e2345bba38ea513
Khai Tran <tttkhai93@gmail.com>
2021-01-13 21:29:59 -0500

    use Formik to build form and handle validation

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,62 @@
 import '../App.css';
-import AddProductForm from './AddProductForm';
 import Cart from './Cart';
 import WishList from './WishList';
-import { useSelector } from 'react-redux';
+import { withFormik, Form, Field } from "formik";
+import { connect } from "react-redux";
+import { useSelector } from "react-redux";
 
-const App = ()=> {
-  const {wishList, cart} = useSelector(state=>({...state}))
-
+const App = ({ errors }) => {
+  const { wishList, cart } = useSelector((state) => ({ ...state }));
   return (
     <div className="App">
-      <h1>Shopping Cart</h1>
-      <AddProductForm wishList={wishList} cart={cart} />
-      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '30px'}}>
-        <WishList wishList={wishList} cart={cart}/>
+      <Form>
+        <Field type="text" name="product" placeholder="Add a product" />
+        <div style={{ color: "red" }}> {errors.product} </div>
+        <button type="submit">Add</button>
+      </Form>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "30px"
+        }}
+      >
+        <WishList wishList={wishList} cart={cart} />
         <Cart wishList={wishList} cart={cart} />
       </div>
-      <div></div>
     </div>
   );
-  
+};
+
+const FormikApp = withFormik({
+  mapPropsToValues({ product }) {
+    return {
+      product: product || ""
+    };
+  },
+  handleSubmit(values, { props, setErrors, resetForm }) {
+    if (values.product === "") {
+      return;
+    }
+
+    if (
+      props.wishList.includes(values.product) ||
+      props.cart.includes(values.product)
+    ) {
+      setErrors({ product: "The product has been added" });
+      return;
+    }
+
+    props.wishList.push(values.product);
+
+    resetForm();
+  }
+})(App);
+
+function mapStateToProps({ cart, wishList }) {
+  return { cart, wishList };
 }
 
-export default (App);
+export default connect(mapStateToProps, null)(FormikApp);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as reduxForm } from 'redux-form';
 import { cartReducer } from './cartReducer';
 import { wishlistReducer } from './wishListReducer';
 
 export const rootReducer = combineReducers({
-    form: reduxForm,
     wishList: wishlistReducer,
     cart: cartReducer
 });
